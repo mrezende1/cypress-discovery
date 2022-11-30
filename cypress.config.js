af2b26e7dcca88cdea56b427d946a82785436052
Mateus Rezende <70781361+mrezende1@users.noreply.github.com>
2022-11-30 17:54:30 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,11 @@ const {
   defineConfig
 } = require("cypress");
 module.exports = defineConfig({
+  projectId: "jdpoor",
   e2e: {
     setupNodeEvents(on, config) {},
     viewportWidth: 1440,
     viewportHeight: 900,
     baseUrl: "https://buger-eats.vercel.app",
-    projectId: "jdpoor"
   },
 });
